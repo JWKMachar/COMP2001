e2e826b2929cd5e190d07193df7839d5819c8aa5
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-01-20 12:39:36 +0000

    Delete function calls Stored Procedures

---
+++ API/COMP2001API(MVC)/COMP2001API(MVC)/COMP2001API(MVC)/Models/VisitsView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace COMP2001API_MVC_.Models
+{
+    public partial class VisitsView
+    {
+        public int UserId { get; set; }
+        public int? UserViewCount { get; set; }
+    }
+}
