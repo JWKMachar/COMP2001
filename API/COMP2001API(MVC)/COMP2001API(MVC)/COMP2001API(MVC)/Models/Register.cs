d50c2f2f8935d2f70b14cf9143df160d70c776ba
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-01-20 14:03:33 +0000

    Stored Procedures called from API
    Register and Update User Stored Procedures are now called from the API

---
+++ API/COMP2001API(MVC)/COMP2001API(MVC)/COMP2001API(MVC)/Models/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2001API_MVC_.Models
+{
+    public class Register
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public string currentPassword { get; set; }
+
+    }
+}
---
+++ API/COMP2001API(MVC)/COMP2001API(MVC)/COMP2001API(MVC)/Models/UpdateUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2001API_MVC_.Models
+{
+    public class UpdateUser
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string CurrentPassword { get; set; }
+
+
+    }
+}
