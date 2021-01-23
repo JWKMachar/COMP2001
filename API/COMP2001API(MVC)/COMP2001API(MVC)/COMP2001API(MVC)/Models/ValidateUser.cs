33d74b1e08e48481793dc9176b8db90a17422307
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-01-23 17:57:01 +0000

    Accidently deselected these in the last push
    oops

---
+++ API/COMP2001API(MVC)/COMP2001API(MVC)/COMP2001API(MVC)/Models/ValidateUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2001API_MVC_.Models
+{
+    public class ValidateUser
+    {
+        public string EmailAddress { get; set; }
+        public string CurrentPassword { get; set; }
+
+    }
+}
